619ff1d035cb001f4c3a1511cb4f5205ac4f74b3
Nguyen Hoang Vu <ecraft.eventagency@gmail.com>
2021-01-08 12:12:20 +0700

    minor

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -4,7 +4,7 @@ import client.handler.NetworkEvtListener;
 public class Client {
   public static void main(String[] args) throws InterruptedException {
     MessageHandler handler = new MessageHandler(new NetworkEvtListener());
-    BettySocket.connect("a894df1985ef048f88bda6af7bd93b09-58679745.ap-southeast-1.elb.amazonaws.com", 8888, handler);
+    BettySocket.connect("a15a8265ed8044d5c9d374b662a7a552-890398595.ap-southeast-1.elb.amazonaws.com", 8888, handler);
     Thread.sleep(10000);
   }
 }
